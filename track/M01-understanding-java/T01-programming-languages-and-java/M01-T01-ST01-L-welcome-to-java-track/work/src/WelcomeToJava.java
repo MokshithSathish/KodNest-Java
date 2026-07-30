27fcf6d0b0a34791ae81a89b0783591f20b04571
mokshith <smokshith590@gmail.com>
2026-07-30 12:57:49 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,9 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        System.out.printlnp("Welcome to the Java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now I am here to learn Java");
     }
 }
