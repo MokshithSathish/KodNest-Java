6d6bae20ee7cc63be0a8d307c4cb7c586c2f90f9
mokshith <smokshith590@gmail.com>
2026-08-04 18:39:35 +0530

    Add Day02 Java practice programs

---
+++ Day02/day2.java
@@ -0,0 +1,11 @@
+
+public class day2 {
+
+    public static void main(String[] args) {
+        System.out.println("Monday");
+        System.out.println("TuesdayWednesday");
+        System.out.println("Thursday");
+        System.out.println("FridaySaturday");
+        System.out.println("Sunday");
+    }
+}
