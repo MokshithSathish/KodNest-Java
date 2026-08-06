fd702a39719123a6672e28d58e78c49195cc63bd
mokshith <smokshith590@gmail.com>
2026-08-06 23:31:54 +0530

    Add Day02 nested1 example

---
+++ Day02/nested1.java
@@ -0,0 +1,14 @@
+
+public class nested1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 2; i++) {
+            for (int j = 1; j <= 2; j++) {
+                if (j == 2) {
+                    break;
+                }
+                System.out.println("i : " + i + "j: " + j);
+            }
+        }
+    }
+}
