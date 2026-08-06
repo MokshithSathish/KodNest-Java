3521d5752b0f94932334f5e43357ecadfa6c3f34
mokshith <smokshith590@gmail.com>
2026-08-06 11:41:30 +0530

    loop

---
+++ OneDrive/Desktop/project/KodNest-Javapractice/track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/M02-introduction to Java/T01-P01/pg2.java
@@ -0,0 +1,10 @@
+
+public class pg2 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int res = a++ + ++a + a++ + a++ + ++a + a++;
+        System.out.println(res);
+    }
+
+}
